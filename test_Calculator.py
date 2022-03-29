3fa27d5e4c273e0107267d12b485b59b4e09b4fb
sumanth1231807 <sumanthravi11111@gmail.com>
2022-03-29 10:38:54 +0530

    calculator

--- test_Calculator.py
+++ test_Calculator.py
@@ -1,27 +1,22 @@
 import Calculator
-def test_add():
-    x=2
-    y=6
-    result = Calculator.add2no(x,y)
-    assert  result==x+y
+import pytest
 
+@pytest.mark.parametrize("x,y,z",[(2,6,5),(7,3,6),(0,9,0)])
+def test_add(x,y,z):
+    result=Calculator.add2no(x,y)
+    assert result == z
 
-def test_sub():
-    x = 2
-    y = 6
-    result = Calculator.sub2no(x, y)
-    assert result == x - y
+@pytest.mark.parametrize("x,y,z",[(6,9,0),(3,8,2),(3,9,9)])
+def test_sub(x,y,z):
+    result=Calculator.sub2no(x,y)
+    assert result == z
 
+@pytest.mark.parametrize("x,y,z",[(3,2,2),(1,9,3)])
+def test_mul(x,y,z):
+    result=Calculator.mul2no(x,y)
+    assert result == z
 
-def test_mul():
-    x = 2
-    y = 6
-    result = Calculator.mul2no(x, y)
-    assert result == x*y
-
-
-def test_div():
-    x = 2
-    y = 6
-    result = Calculator.div2no(x, y)
-    assert result == x / y
+@pytest.mark.parametrize("x,y,z",[(0,0,1),(2,1,9)])
+def test_div(x,y,z):
+    result=Calculator.div2no(x,y)
+    assert result == z
